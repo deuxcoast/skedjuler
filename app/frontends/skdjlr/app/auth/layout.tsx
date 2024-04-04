d4c3d929eeacf311ea7c59c2f1219acbf9d8fc1f
duexcoast <conor.ux@gmail.com>
2024-04-04 18:04:01 -0400

    background image on login screen

--- app/frontends/skdjlr/app/auth/layout.tsx
+++ app/frontends/skdjlr/app/auth/layout.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import gradientBackground from "../../public/noise-gradient.jpg";
 
 export default function AuthLayout({
   children,
@@ -14,11 +11,9 @@ export default function AuthLayout({
       <div className="flex items-center justify-center py-12">{children}</div>
       <div className="hidden bg-muted lg:block">
         <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          src={gradientBackground}
+          alt="Pastel gradient"
+          className="h-full w-full left-1/2 bg-cover bg-center"
         />
       </div>
     </div>
