edaec69fab97d3e31931e548592e8b01db05cb31
duexcoast <conor.ux@gmail.com>
2024-04-23 21:34:14 -0400

    add "hello-pangea/dnd" drag-and-drop dependency

--- app/frontends/skdjlr/app/(scheduler)/scheduler/page.tsx
+++ app/frontends/skdjlr/app/(scheduler)/scheduler/page.tsx
@@ -1,9 +1,35 @@
 import WeekGrid from "@/components/scheduler/WeekGrid";
+import { DragDropContext } from "@hello-pangea/dnd";
 
 export default function SchedulerPage() {
+  // using useCallback is optional
+  const onBeforeCapture = useCallback(() => {
+    /*...*/
+  }, []);
+  const onBeforeDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragUpdate = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragEnd = useCallback(() => {
+    // the only one that is required
+  }, []);
+
   return (
     <div className="m-4 basis-full">
-      <WeekGrid />
+      <DragDropContext
+        onBeforeCapture={onBeforeCapture}
+        onBeforeDragStart={onBeforeDragStart}
+        onDragStart={onDragStart}
+        onDragUpdate={onDragUpdate}
+        onDragEnd={onDragEnd}
+      >
+        <WeekGrid />
+      </DragDropContext>
     </div>
   );
 }
