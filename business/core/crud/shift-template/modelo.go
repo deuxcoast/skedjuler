6d9cb97e166e5e31d1ec83c81d763c26b1268991
duexcoast <conor.ux@gmail.com>
2024-05-17 01:37:23 -0700

    shifttemplate core data model

---
+++ business/core/crud/shift-template/modelo.go
@@ -0,0 +1,26 @@
+package shifttemplate
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type ShiftTemplate struct {
+	Id        uuid.UUID
+	RoleId    uuid.UUID
+	StartTime time.Time
+	EndTime   time.Time
+}
+
+type NewShiftTemplate struct {
+	Id        uuid.UUID
+	RoleId    uuid.UUID
+	StartTime time.Time
+	EndTime   time.Time
+}
+type UpdateShiftTemplate struct {
+	RoleId    *uuid.UUID
+	StartTime *time.Time
+	EndTime   *time.Time
+}
